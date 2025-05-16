764391f127a84cc2cb094fd4519669d7aa4923c1
T-Man <167492062+Tmangoz@users.noreply.github.com>
2025-05-16 13:26:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,19 @@ csvread = pd.read_csv(csv_file)
 StocksDF = pd.DataFrame(csvread)
 
 st.title('Team Alpha Stock Final Project')
+Answer = str(st.text_input('Please Enter a stock you would like to see(NVDA,AAPL,GOOGL, and AMZN):'))
+Answer2 = str(st.text_input('Please enter the date of that stock you would like to view(M/D/Y):'))
 
-st.dataframe(StocksDF['Date'])
+cAnswer = Answer.upper()
+cAnswer2 = Answer.upper()
+if st.button("Search"):
+  if cAnswer  == 'NVDA':
+    result = StocksDF.loc[StocksDF.Date == cAnswer2,['Open_NVDA', 'Close_NVDA']] 
+    st.dataframe(result)
+  else:
+    print('Please enter a valid stock or date')
+else:
+  print('Please enter a valid stock or date')
+
 
 
