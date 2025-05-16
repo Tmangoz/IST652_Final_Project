482595a559fcd0e3a88f0e248aca08affabdc9a4
T-Man <167492062+Tmangoz@users.noreply.github.com>
2025-05-16 13:00:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np  
+import matplotlib.pyplot as plt
+import yfinance as yf
+import csv
+csv_file = '15 Years Stock Data of NVDA AAPL MSFT GOOGL and AMZN.csv'
+# Load the CSV file into a DataFrame    
+csvread = pd.read_csv(csv_file)
+StocksDF = pd.DataFrame(csvread)
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title('Team Alpha Stock Final Project')
+
