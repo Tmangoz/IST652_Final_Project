3838d49dbc508d9625a936e621f6d8e0c03af6ee
T-Man <167492062+Tmangoz@users.noreply.github.com>
2025-05-16 13:24:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 import csv
-csv_file = '15 Years Stock Data of NVDA AAPL MSFT GOOGL and AMZN.csv'
+csv_file = "15 Years Stock Data of NVDA AAPL MSFT GOOGL and AMZN.csv"
 # Load the CSV file into a DataFrame    
 csvread = pd.read_csv(csv_file)
 StocksDF = pd.DataFrame(csvread)
