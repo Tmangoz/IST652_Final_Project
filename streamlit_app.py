ae9fef5c6a3bbc4eabb581b705f11eb149763be6
T-Man <167492062+Tmangoz@users.noreply.github.com>
2025-05-16 13:21:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ csvread = pd.read_csv(csv_file)
 StocksDF = pd.DataFrame(csvread)
 
 st.title('Team Alpha Stock Final Project')
+print(StocksDF.Date.head())
 
 Answer = str(st.text_input('Please Enter a stock you would like to see(NVDA,AAPL,GOOGL, and AMZN):'))
 Answer2 = str(st.text_input('Please enter the date of that stock you would like to view(M/D/Y):'))
